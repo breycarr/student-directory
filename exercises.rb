e5157c34a1404fe4476455017c392d72aea4c4fe
breycarr <bjr.kitz@gmail.com>
2019-04-23 21:23:23 +0000

    copied directory.rb to make exercises.rb, used for excercises in chapter 8, completed exercise 1, print student number before name

--- exercises.rb
+++ exercises.rb
@@ -21,16 +21,9 @@ def print_header
 end
 
 def print(students)
-counter = 0 
-  while counter <= students.last.index
-    puts "#{counter + 1} #{student[counter[:name]]} (#{student[counter[:cohort]]} cohort)"
-    counter += 1
-  end    
-=begin  
   students.each_with_index do |student, number|
     puts "#{number + 1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
-=end
 end
 
 def print_footer(students)
