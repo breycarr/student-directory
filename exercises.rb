f31f030d2e652a3d4b9900f85420f25bcf883b43
breycarr <bjr.kitz@gmail.com>
2019-04-24 19:23:11 +0000

    added more information to hash. Centered output

--- exercises.rb
+++ exercises.rb
@@ -5,9 +5,11 @@ def input_students
   students = []
   
   name = gets.chomp
+  location = gets.chomp
+  club = gets.chomp
   
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, place_of_birth: location, club: club}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
@@ -16,20 +18,20 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villain Academy"
-  puts "-------------"
+  puts "The students of Villain Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   students.each do |student|
     if student[:name].length <= 12
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
     end
   end    
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
@@ -41,8 +43,8 @@ print_footer(students)
 # Exercise 2 - Done
 # Exercise 3 - Done
 # Exercise 4
-# Exercise 5
-# Exercise 6
+# Exercise 5 - Done
+# Exercise 6 - Done
 # Exercise 7
 # Exercise 8
 # Exercise 9
