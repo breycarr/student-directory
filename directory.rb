f1e33a94e65ba9db99cee356addefcf86e27c6d2
breycarr <bjr.kitz@gmail.com>
2019-04-24 20:02:22 +0000

    refactored methods for greater readability, teased out bulk processes into more dedicated methods

--- directory.rb
+++ directory.rb
@@ -1,44 +1,48 @@
+@students = []
+
 def interactive_menu
-  students = []
-  
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    
-    selection = gets.chomp
-    
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit
-      else
-        puts "I don't know what you mean, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   
-  students = []
-  
   name = gets.chomp
   
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  
-  students
+end
+
+def show_students
+  print_header
+  print_student_list(@students)
+  print_footer(@students)
 end
 
 def print_header
@@ -46,14 +50,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student, number|
+def print_student_list(students)
+  @students.each do |student, number|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
