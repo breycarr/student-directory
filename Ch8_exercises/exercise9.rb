ce261261627a4150fe9517f1906058673702fb79
breycarr <bjr.kitz@gmail.com>
2019-04-25 10:41:54 +0000

    Corrected strings to account for singular as well as plurals

--- Ch8_exercises/exercise9.rb
+++ Ch8_exercises/exercise9.rb
@@ -1,3 +1,7 @@
+# Right now if we have only one student, the user will see a message "Now we have 1 students", 
+# whereas it should be "Now we have 1 student". How can you fix it so that it used singular form 
+# when appropriate and plural form otherwise?
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -8,7 +12,11 @@ def input_students
 
   while !name.empty? do
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     name = gets.chomp
   end
 
@@ -27,7 +35,11 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 students = input_students
