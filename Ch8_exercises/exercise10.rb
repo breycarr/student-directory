4083d412b035edc6125a12e5f9049b45ab291b23
breycarr <bjr.kitz@gmail.com>
2019-04-25 09:56:22 +0000

    created folder for Chapter 8 exercises with duplicate files, completed exercise 1

--- Ch8_exercises/exercise10.rb
+++ Ch8_exercises/exercise10.rb
@@ -1,19 +1,17 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
+
   students = []
-  
+
   name = gets.chomp
-  cohort = gets.chomp.to_sym
-  
+
   while !name.empty? do
-    students << {name: name, cohort: cohort}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     name = gets.chomp
-    cohort = gets.chomp.to_sym
   end
-  
+
   students
 end
 
@@ -25,7 +23,7 @@ end
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end    
+  end
 end
 
 def print_footer(students)
@@ -36,16 +34,3 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
-
-# Exercise 1 - Done
-# Exercise 2 - Done
-# Exercise 3 - Done
-# Exercise 4
-# Exercise 5 - Done
-# Exercise 6 - Done
-# Exercise 7
-# Exercise 8
-# Exercise 9
-# Exercise 10
-# Exercise 11
-# Exercise 12
