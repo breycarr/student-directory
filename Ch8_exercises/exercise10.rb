f8968cf8c2ee4e7a79a89a7c6caa858396d070e6
breycarr <bjr.kitz@gmail.com>
2019-04-25 10:47:34 +0000

    Can use gsub with regexp instead of chomp (can also use strip, but that removes all white space)

--- Ch8_exercises/exercise10.rb
+++ Ch8_exercises/exercise10.rb
@@ -1,15 +1,19 @@
+# We've been using the chomp() method to get rid of the last return character. 
+# Find another method among those provided by the String class that could be used 
+# for the same purpose (although it will require passing some arguments).
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
   students = []
 
-  name = gets.chomp
+  name = gets.gsub(/\n/, "")
 
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    name = gets.gsub(/\n/, "")
   end
 
   students
